ab7fd79350c7c3d6589d9dcbae1a988fc7f7be3e
unknown <mahsuma.rezai8@gmail.com>
2025-01-24 04:38:35 -0800

    work on the main

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import { Fragment } from 'react';
 import { About } from './Components/About';
 import Head from './Components/Head';
+import { Main } from './Components/Main';
 
 const App = (props) => {
     return (
         <Fragment>
             <Head/>
+            <Main/>
             <About/>
 
         </Fragment>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,17 +1,21 @@
 import { Fragment } from 'react'
+import { Button } from './Button';
+import classes from './Main.module.css'
 export const Main = (props) => {
     return (
         <Fragment>
-            <div>
+            <div className={classes.main}>
                 <div>
                     <p>Welcome to my portfolio website!</p>
                     <h1>Hey folks, I'm Marvel SannWeb DesignerUI Specialist</h1>
                     <span>Building a successful product is a challenge. I am highly energetic in user experience design,
                          interfaces and web development.</span>
-                         
+
+
 
                 </div>
                 <div>
+                    <Button />
 
                 </div>
             </div>
